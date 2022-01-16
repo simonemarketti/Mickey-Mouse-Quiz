b56dc69dca835fe0c24447b0fc57eb25ad056462
simonemarketti <simomarketti@icloud.com>
2022-01-16 19:44:35 +0100

    Added stuff and images

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //  query form and result
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
+const images = document.querySelectorAll('.stuff');
 
 //  correct answers
 const correctAnswers = ['A', 'B', 'B', 'A'];
@@ -9,25 +10,24 @@ const correctAnswers = ['A', 'B', 'B', 'A'];
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  // save the answers
+  //  save the answers
   const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
-  // create the score
+  //  create the score
   let score = 0;
 
   //  check the answers
   userAnswers.forEach((answer, index) => {
     if (answer === correctAnswers[index]) {
       score += 25;
+      images[index].classList.remove('d-none')
     }
   });
 
-  // show the result
-  scrollTo(0, 0);
-  result.classList.remove('d-none');
-
+  //  animation
+  //  create a counter
   let counter = 0;
-
+  //  create animation
   const timer = setInterval(() => {
     result.querySelector('.percentage').textContent = `${counter}%`;
     if (counter === score) {
@@ -36,4 +36,8 @@ form.addEventListener('submit', e => {
       counter++;
     }
   }, 70);
+
+  // show the result
+  scrollTo(0, 0);
+  result.classList.remove('d-none');
 });
