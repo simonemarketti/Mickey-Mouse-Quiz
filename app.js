b5defc21e41396dc63c95f0986efd08ca2078c9c
simonemarketti <simomarketti@icloud.com>
2022-01-17 12:31:12 +0100

    Changed timer and images

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 //  query form and result
+const quiz = document.querySelector('.quiz')
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
+const percentage = document.querySelector('.percentage')
 const images = document.querySelectorAll('.stuff');
+const errors = document.querySelectorAll('.error')
+const totalAnswers = document.querySelector('.total-answers');
+
 
 //  correct answers
 const correctAnswers = ['A', 'B', 'B', 'A'];
@@ -10,33 +15,51 @@ const correctAnswers = ['A', 'B', 'B', 'A'];
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  //  save the answers
+  //  save the user's answers
   const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
   //  create the score
   let score = 0;
+  let scoreAnswers = 0;
 
   //  check the answers
   userAnswers.forEach((answer, index) => {
     if (answer === correctAnswers[index]) {
       score += 25;
+      scoreAnswers += 1;
       images[index].classList.remove('d-none')
+      errors[index].classList.add('d-none')
     }
   });
 
-  // animation - create a counter and setInterval
-  let counter = 0;
+  //  refresh number answers
+  totalAnswers.textContent = scoreAnswers;
+
+  // animation - create a random counter animation and set ripetition 
+  let randomNumber = 0;
+  let timerRepetitions = 20;
 
   const timer = setInterval(() => {
-    result.querySelector('.percentage').textContent = `${counter}%`;
-    if (counter === score) {
+    percentage.textContent = `${randomNumber}%`;
+    if (timerRepetitions === 0) {
+      //  conditional formatting result
+      if (score < 25) {
+        percentage.classList.remove('text-warning');
+        percentage.classList.add('text-danger');
+      } else if (score > 75) {
+        percentage.classList.remove('text-warning');
+        percentage.classList.add('text-success');
+      }
+      percentage.textContent = `${score}%`;
       clearInterval(timer);
     } else {
-      counter++;
+      randomNumber = Math.floor(Math.random() * 100);
+      timerRepetitions--;
     }
   }, 70);
 
   // show the result
   scrollTo(0, 0);
   result.classList.remove('d-none');
+  quiz.classList.add('d-none');
 });
